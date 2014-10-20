a0f60f7348c5a1e27b345f19a4087d2aa4895215
adisorn <armchockaumnuai@gmail.com>
2014-10-20 13:48:21 +0800

    mocking time service and test not holiday

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -4,12 +4,23 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class SchedulerTest {
 
     @Test
     public void testCreate(){
         Date now = new Date();
         new TestingScheduler("Bill", new FakeDisplay()).addEvent(new Event(now, DayTime.Time10AM));
+    }
+
+    @Test
+    public void sholdReturnsMeetingWhenNotHoliday(){
+        Date now = new Date();
+        Scheduler scheduler = new TestingScheduler("Bill", new FakeDisplay());
+        scheduler.addEvent(new Meeting(now, DayTime.Time10AM, "meeting"));
+        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, new FakeTimeService()).getText(), is("meeting"));
     }
 }
 
@@ -26,5 +37,11 @@ class TestingScheduler extends Scheduler{
     }
 
     protected void sendMail(String address, String subject, String message){
+    }
+}
+
+class FakeTimeService extends TimeServices{
+    public boolean isDateAHoliday(Date date) {
+        return false;
     }
 }
