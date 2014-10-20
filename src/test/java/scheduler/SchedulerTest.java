ea425626b8f8fd5a67cc98d70a9aafda19dfc8b5
adisorn <armchockaumnuai@gmail.com>
2014-10-20 14:07:20 +0800

    refactor time service variable

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -10,60 +10,59 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SchedulerTest {
 
+    TimeServices holidayTimeService = new TimeServices() {
+        public boolean isDateAHoliday(Date date) {
+            return true;
+        }
+    };
+
+    TimeServices nonHolidayTimeService = new TimeServices() {
+        public boolean isDateAHoliday(Date date) {
+            return false;
+        }
+    };
+
     @Test
-    public void testCreate(){
+    public void testCreate() {
         Date now = new Date();
         new TestingScheduler("Bill", new FakeDisplay()).addEvent(new Event(now, DayTime.Time10AM));
     }
 
     @Test
-    public void shouldReturnsMeetingWhenNotHoliday(){
+    public void shouldReturnsMeetingWhenNotHoliday() {
         Date now = new Date();
         Scheduler scheduler = new TestingScheduler("Bill", new FakeDisplay());
         scheduler.addEvent(new Meeting(now, DayTime.Time10AM, "meeting"));
-        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, new NotHolidayTimeService()).getText(), is("meeting"));
+        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, nonHolidayTimeService).getText(), is("meeting"));
     }
 
     @Test
-    public void shouldNotReturnsMeetingWhenHoliday(){
+    public void shouldNotReturnsMeetingWhenHoliday() {
         Date now = new Date();
         Scheduler scheduler = new TestingScheduler("Bill", new FakeDisplay());
         scheduler.addEvent(new Meeting(now, DayTime.Time10AM, "meeting"));
-        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, new HolidayTimeService()), is(nullValue()));
+        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, holidayTimeService), is(nullValue()));
     }
 
     @Test
-    public void shouldNotReturnsMeetingWhenNeverAddEvent(){
+    public void shouldNotReturnsMeetingWhenNeverAddEvent() {
         Date now = new Date();
         Scheduler scheduler = new TestingScheduler("Bill", new FakeDisplay());
-        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, new HolidayTimeService()), is(nullValue()));
+        assertThat(scheduler.getMeeting(now, DayTime.Time10AM, holidayTimeService), is(nullValue()));
     }
 }
 
 class FakeDisplay implements Display {
-
     public void showEvent(Event event) {
 
     }
 }
 
-class TestingScheduler extends Scheduler{
-    public TestingScheduler(String owner, Display display){
+class TestingScheduler extends Scheduler {
+    public TestingScheduler(String owner, Display display) {
         super(owner, display);
     }
 
-    protected void sendMail(String address, String subject, String message){
-    }
-}
-
-class NotHolidayTimeService extends TimeServices{
-    public boolean isDateAHoliday(Date date) {
-        return false;
-    }
-}
-
-class HolidayTimeService extends TimeServices{
-    public boolean isDateAHoliday(Date date) {
-        return true;
+    protected void sendMail(String address, String subject, String message) {
     }
 }
